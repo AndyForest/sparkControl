51490a990d6315573c0c84094153ba708e038b7b
AndyForest <andy@makerkids.ca>
2015-01-25 19:23:11 -0500

    resolving cross domain error

--- js/sparkControl.js
+++ js/sparkControl.js
@@ -115,12 +115,14 @@ sparkControl.prototype.logTestMessage = function(message) {
 
 function sparkControlTestMessage (message) {
 	console.log(message);
-	messageWindow = $("#SparkCoreMessageWindow");
+	//messageWindow = $("#SparkCoreMessageWindow");
 	if (messageWindow.length) {
 		// Message window exists, output message
-		messageWindow.text(message);
+		//messageWindow.text(message);
 	}
 }
+
+
 
 
 /* Doesn't work. Possibly need to call an existing function instead of creating a new one on the fly like this.
